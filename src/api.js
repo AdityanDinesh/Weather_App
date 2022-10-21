68268088d2bb043cd37baa24267da87613ced14d
adityandinesh <adityandinesh001@gmail.com>
2022-10-21 20:41:41 +0530

    inital commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '42f16c845bmsh7657ab319704d94p10ce63jsn730dead6cb53',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
